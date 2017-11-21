d59748a6b50bf68ef95aedce87e524a64b0766a1
parvez <parvez.patel@sjsu.edu>
2017-11-21 06:17:26 -0800

    rds details added

--- Server/config/config.js
+++ Server/config/config.js
@@ -2,10 +2,10 @@ module.exports = {
   "development": {
     //TODO: replace this with actual database
     database:{
-      "username": "consumesafe", 
-      "password": "ConsumeSafe123",
-      "database": "consumesafedb",
-      "host" : "consumesafe.cb17gye1ruth.us-west-1.rds.amazonaws.com",
+      "username": "cmpe277", 
+      "password": "Zhang27717",
+      "database": "libarymanagementdb",
+      "host" : "cmpe277.crkpwroxroee.us-west-1.rds.amazonaws.com",
       "port": "5432",
       "dialect": "postgres"
    }
