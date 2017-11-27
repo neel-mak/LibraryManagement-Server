352807ff51e41a617ebcefdd3d1653aa36c6e48d
NEEL PATEL <neelpatel3284@gmail.com>
2017-11-26 16:10:37 -0800

    Verification code UI added.

---
+++ neel-code/AndroidApp/app/src/main/java/com/librarymanagement/siddharth/snaplibrary/Confirmation_Fragment.java
@@ -0,0 +1,29 @@
+package com.librarymanagement.siddharth.snaplibrary;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Confirmation_Fragment extends Fragment {
+
+
+    public Confirmation_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirmation_, container, false);
+    }
+
+}
