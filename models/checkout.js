13dbfe4fac8ca39be00bb1d8cff4715e2e90e7bd
parvez <parvez.patel@sjsu.edu>
2017-11-28 11:29:04 -0800

    checkout model added

---
+++ models/checkout.js
@@ -0,0 +1,25 @@
+//TODO: replace with actual model
+
+
+var Sequelize = require('sequelize');
+
+module.exports = function(sequelize, DataTypes) {
+
+    var Checkout = sequelize.define('Checkout', {
+        bookId: {type: Sequelize.STRING, field: 'book_id'},
+        patronId: {type: Sequelize.INTEGER, field: 'patron_id'},
+        checkoutDate: {type: Sequelize.DATE, field: 'checkout_date'},
+        dueDate: {type: Sequelize.DATE, field: 'due_date'},
+        renewCount:{type: Sequelize.INTEGER, field: 'renew_count'},
+        currentFine: {type: Sequelize.INTEGER, field: 'current_fine'}
+    },{
+        tableName: 'checkouts',
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    });
+        
+
+    return Checkout;
+
+};
